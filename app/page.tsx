0d0922d02b32fc5fa446024737138d0c0a4440f4
Josh Kenyon <joshuakenyon1@gmail.com>
2025-08-12 16:29:27 +0100

    even more fixes

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,11 @@ import ListingCard from "./components/listings/ListingCard";
 import getCurrentUser from "./actions/getCurrentUser";
 
 interface HomeProps {
-  searchParams: Partial<IListingsParams>;
+  searchParams?: Partial<IListingsParams>;
 }
 
-const Home = async ( {searchParams} : HomeProps )=> {
-  const listings = await getListings(searchParams);
+const Home = async ({ searchParams }: HomeProps) => {
+  const listings = await getListings(searchParams ?? {});
   const currentUser = await getCurrentUser();
 
   if (listings.length === 0) {
@@ -21,23 +21,27 @@ const Home = async ( {searchParams} : HomeProps )=> {
     <Container>
       <div
         className="
-      pt-8
-      grid
-      grid-cols-1
-      sm:grid-cols-2
-      md:grid-cols-3
-      lg:grid-cols-4
-      xl:grid-cols-5
-      2xl:grid-cols-6
-      gap-8
-      "
+          pt-8
+          grid
+          grid-cols-1
+          sm:grid-cols-2
+          md:grid-cols-3
+          lg:grid-cols-4
+          xl:grid-cols-5
+          2xl:grid-cols-6
+          gap-8
+        "
       >
-        {listings.map((listing) => {
-          return <ListingCard key={listing.id} data={listing} currentUser={currentUser}/>;
-        })}
+        {listings.map((listing) => (
+          <ListingCard
+            key={listing.id}
+            data={listing}
+            currentUser={currentUser}
+          />
+        ))}
       </div>
     </Container>
   );
-}
+};
 
 export default Home;
