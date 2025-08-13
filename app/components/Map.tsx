30eca51b09f45a0502fcd12aa884892a405f64bc
Josh Kenyon <joshuakenyon1@gmail.com>
2025-08-13 12:53:09 +0100

    added zoom feature on map in rent modal and moved marker images to public leaflet folder

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -1,33 +1,35 @@
 "use client";
 
 import L from "leaflet";
-import { MapContainer, Marker, TileLayer } from "react-leaflet";
-
+import { MapContainer, Marker, TileLayer, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
-import markerIcon from "leaflet/dist/images/marker-icon.png";
-import marketShadow from "leaflet/dist/images/marker-shadow.png";
-
 
 
 // @ts-expect-error required to override default Leaflet icon URLs
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
-  iconUrl: markerIcon,
-  iconRetinaUrl: markerIcon2x,
-  shadowUrl: marketShadow,
+  iconUrl: "/leaflet/marker-icon.png",
+  iconRetinaUrl: "/leaflet/marker-icon-2x.png",
+  shadowUrl: "/leaflet/marker-shadow.png",
 });
 
 interface MapProps {
   center?: number[];
 }
 
+function ChangeView({ center }: { center: L.LatLngExpression }) {
+  const map = useMap();
+  map.setView(center, 15); // zoom to 15 on location change
+  return null;
+}
+
 const Map: React.FC<MapProps> = ({ center }) => {
+  const defaultCenter: L.LatLngExpression = [-23.55052, -46.633308];
+
   return (
-    
     <MapContainer
-      center={(center as L.LatLngExpression) || [-23.55052, -46.633308]}
-      zoom={center ?  15 : 3}
+      center={(center as L.LatLngExpression) || defaultCenter}
+      zoom={center ? 15 : 3}
       scrollWheelZoom={false}
       className="h-[35vh] rounded-lg"
     >
@@ -35,10 +37,12 @@ const Map: React.FC<MapProps> = ({ center }) => {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-      {center &&
-      <Marker 
-        position={center as L.LatLngExpression}
-      />}
+      {center && (
+        <>
+          <Marker position={center as L.LatLngExpression} />
+          <ChangeView center={center as L.LatLngExpression} />
+        </>
+      )}
     </MapContainer>
   );
 };
